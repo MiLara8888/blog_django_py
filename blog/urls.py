aa5126a08cf260a2d93c6a390200418900a76b91
MiLara8888 <MiLara8888@mail.com>
2021-10-13 13:12:18 +0300

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path  # path для создания путей
 from . import views  # импорт представлений
 
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
